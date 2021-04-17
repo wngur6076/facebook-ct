7f9f6e490170dfc02fe439ff0c841c607591aa23
wngur6076 <wngur6076@naver.com>
2021-04-17 23:48:07 +0900

    모든 이미지가 올바르게 표시되고 업데이트 될 때 다시로드되는지 확인하겠습니다. 그런 다음 UploadableImage 구성 요소에 마무리 작업을 추가하고 인증 된 사용자에게만 표시되도록합니다.

--- public/js/app.js
+++ public/js/app.js
@@ -1971,6 +1971,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /* harmony import */ var lodash__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(lodash__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var vuex__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vuex */ "./node_modules/vuex/dist/vuex.esm.js");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 //
 //
 //
@@ -1998,9 +2005,12 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "NewPost",
-  computed: {
+  computed: _objectSpread(_objectSpread({}, (0,vuex__WEBPACK_IMPORTED_MODULE_1__.mapGetters)({
+    authUser: 'authUser'
+  })), {}, {
     postMessage: {
       get: function get() {
         return this.$store.getters.postMessage;
@@ -2009,7 +2019,7 @@ __webpack_require__.r(__webpack_exports__);
         this.$store.commit('updateMessage', postMessage);
       }, 300)
     }
-  }
+  })
 });
 
 /***/ }),
@@ -2025,6 +2035,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
+//
+//
 //
 //
 //
@@ -2152,6 +2164,13 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var dropzone__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! dropzone */ "./node_modules/dropzone/dist/dropzone.js");
 /* harmony import */ var dropzone__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(dropzone__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var vuex__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! vuex */ "./node_modules/vuex/dist/vuex.esm.js");
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 //
 //
 //
@@ -2162,19 +2181,23 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "UploadableImage",
   props: ['userImage', 'imageWidth', 'imageHeight', 'location', 'classes', 'alt'],
   data: function data() {
     return {
-      dropzone: null,
-      uploadedImage: null
+      dropzone: null
     };
   },
   mounted: function mounted() {
-    this.dropzone = new (dropzone__WEBPACK_IMPORTED_MODULE_0___default())(this.$refs.userImage, this.settings);
+    if (this.authUser.data.user_id.toString() === this.$route.params.userId) {
+      this.dropzone = new (dropzone__WEBPACK_IMPORTED_MODULE_0___default())(this.$refs.userImage, this.settings);
+    }
   },
-  computed: {
+  computed: _objectSpread(_objectSpread({}, (0,vuex__WEBPACK_IMPORTED_MODULE_1__.mapGetters)({
+    authUser: 'authUser'
+  })), {}, {
     settings: function settings() {
       var _this = this;
 
@@ -2191,14 +2214,15 @@ __webpack_require__.r(__webpack_exports__);
           'X-CSRF-TOKEN': document.head.querySelector('meta[name=csrf-token]').content
         },
         success: function success(e, res) {
-          _this.uploadedImage = res;
+          _this.$store.dispatch('fetchAuthUser');
+
+          _this.$store.dispatch('fetchUser', _this.$route.params.userId);
+
+          _this.$store.dispatch('fetchUserPosts', _this.$route.params.userId);
         }
       };
-    },
-    imageObject: function imageObject() {
-      return this.uploadedImage || this.userImage;
     }
-  }
+  })
 });
 
 /***/ }),
@@ -49573,7 +49597,8 @@ var render = function() {
                 staticClass: "w-8 h-8 object-cover rounded-full",
                 attrs: {
                   src:
-                    "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
+                    _vm.authUser.data.attributes.profile_image.data.attributes
+                      .path,
                   alt: "profile image for user"
                 }
               })
@@ -49657,7 +49682,18 @@ var render = function() {
   var _c = _vm._self._c || _h
   return _c("div", { staticClass: "bg-white rounded shadow w-2/3 p-4 " }, [
     _c("div", { staticClass: "flex justify-between items-center" }, [
-      _vm._m(0),
+      _c("div", [
+        _c("div", { staticClass: "w-8" }, [
+          _c("img", {
+            staticClass: "w-8 h-8 object-cover rounded-full",
+            attrs: {
+              src:
+                _vm.authUser.data.attributes.profile_image.data.attributes.path,
+              alt: "profile image for user"
+            }
+          })
+        ])
+      ]),
       _vm._v(" "),
       _c(
         "div",
@@ -49738,25 +49774,7 @@ var render = function() {
     ])
   ])
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", [
-      _c("div", { staticClass: "w-8" }, [
-        _c("img", {
-          staticClass: "w-8 h-8 object-cover rounded-full",
-          attrs: {
-            src:
-              "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
-            alt: "profile image for user"
-          }
-        })
-      ])
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -49785,7 +49803,17 @@ var render = function() {
     [
       _c("div", { staticClass: "flex flex-col p-4" }, [
         _c("div", { staticClass: "flex items-center" }, [
-          _vm._m(0),
+          _c("div", { staticClass: "w-8" }, [
+            _c("img", {
+              staticClass: "w-8 h-8 object-cover rounded-full",
+              attrs: {
+                src:
+                  _vm.post.data.attributes.posted_by.data.attributes
+                    .profile_image.data.attributes.path,
+                alt: "profile image for user"
+              }
+            })
+          ]),
           _vm._v(" "),
           _c("div", { staticClass: "ml-6" }, [
             _c("div", { staticClass: "text-sm font-bold" }, [
@@ -50003,7 +50031,17 @@ var render = function() {
                     staticClass: "flex my-4 items-center"
                   },
                   [
-                    _vm._m(1, true),
+                    _c("div", { staticClass: "w-8" }, [
+                      _c("img", {
+                        staticClass: "w-8 h-8 object-cover rounded-full",
+                        attrs: {
+                          src:
+                            comment.data.attributes.commented_by.data.attributes
+                              .profile_image.data.attributes.path,
+                          alt: "profile image for user"
+                        }
+                      })
+                    ]),
                     _vm._v(" "),
                     _c("div", { staticClass: "ml-4 flex-1" }, [
                       _c(
@@ -50059,37 +50097,7 @@ var render = function() {
     ]
   )
 }
-var staticRenderFns = [
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "w-8" }, [
-      _c("img", {
-        staticClass: "w-8 h-8 object-cover rounded-full",
-        attrs: {
-          src:
-            "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
-          alt: "profile image for user"
-        }
-      })
-    ])
-  },
-  function() {
-    var _vm = this
-    var _h = _vm.$createElement
-    var _c = _vm._self._c || _h
-    return _c("div", { staticClass: "w-8" }, [
-      _c("img", {
-        attrs: {
-          src:
-            "https://cdn.pixabay.com/photo/2014/07/09/10/04/man-388104_960_720.jpg",
-          alt: ""
-        }
-      })
-    ])
-  }
-]
+var staticRenderFns = []
 render._withStripped = true
 
 
@@ -50156,7 +50164,7 @@ var render = function() {
     _c("img", {
       ref: "userImage",
       class: _vm.classes,
-      attrs: { src: _vm.imageObject.data.attributes.path, alt: _vm.alt }
+      attrs: { src: _vm.userImage.data.attributes.path, alt: _vm.alt }
     })
   ])
 }
