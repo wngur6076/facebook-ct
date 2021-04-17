b896d6093683c938e3e28144c800c77a9e03d18e
wngur6076 <wngur6076@naver.com>
2021-04-18 00:01:07 +0900

    composer require intervention/image 설치하여 이미지 리사이즈

--- public/js/app.js
+++ public/js/app.js
@@ -50266,8 +50266,8 @@ var render = function() {
               [
                 _c("UploadableImage", {
                   attrs: {
-                    "image-width": "1500",
-                    "image-height": "300",
+                    "image-width": "1200",
+                    "image-height": "500",
                     location: "cover",
                     alt: "user background image",
                     classes: "object-cover w-full",
@@ -50291,8 +50291,8 @@ var render = function() {
                   [
                     _c("UploadableImage", {
                       attrs: {
-                        "image-width": "1500",
-                        "image-height": "300",
+                        "image-width": "750",
+                        "image-height": "750",
                         location: "profile",
                         alt: "user profile image",
                         classes:
